9ccf2e0d6f6eb7e5ddeb9e151093a8c4ef32bf03
Prabhat-2000 <102291706+Prabhat-2000@users.noreply.github.com>
2022-07-28 21:26:29 +0530

    Add files via upload

---
+++ MegaByteConverter.java
@@ -0,0 +1,16 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int KiloBytes){
+            if (KiloBytes < 0){
+                System.out.println("Invalid Value");
+            }
+            else {
+                int megabytes = KiloBytes / 1024;
+                
+                    System.out.println(KiloBytes + " KB = "+ megabytes + " MB and " + KiloBytes%1024 + " KB");
+                
+
+            }
+
+        }
+ 
+}
